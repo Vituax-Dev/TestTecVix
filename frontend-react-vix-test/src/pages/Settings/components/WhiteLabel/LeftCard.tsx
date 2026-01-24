4b1eb49550774635e565cbb7a228a49afdab8543
Rodrigo <clancoto2015@hotmail.com>
2026-01-24 18:17:13 -0300

    feat: add Vituax user restrictions for editing company settings in WhiteLabel components

--- frontend-react-vix-test/src/pages/Settings/components/WhiteLabel/LeftCard.tsx
+++ frontend-react-vix-test/src/pages/Settings/components/WhiteLabel/LeftCard.tsx
@@ -2,6 +2,8 @@ import { Divider, Stack } from "@mui/material";
 import { themeColors, useZTheme } from "../../../../stores/useZTheme";
 import { LeftCardLogo } from "./LeftCardLogo";
 import { LeftCardDomain } from "./LeftCardDomain";
+import { useZUserProfile } from "../../../../stores/useZUserProfile";
+import { useZBrandInfo } from "../../../../stores/useZBrandStore";
 
 interface IWhiteLabelChildProps {
   theme: {
@@ -11,6 +13,10 @@ interface IWhiteLabelChildProps {
 }
 export const LeftCard = ({ theme }: IWhiteLabelChildProps) => {
   const { mode } = useZTheme();
+  const { role } = useZUserProfile();
+  const { idBrand } = useZBrandInfo();
+  const isVituaxUser = idBrand === null;
+  const isAdmin = role === "admin" && !isVituaxUser;
   return (
     <Stack
       sx={{
@@ -25,9 +31,9 @@ export const LeftCard = ({ theme }: IWhiteLabelChildProps) => {
         "@media (max-width: 1000px)": { width: "100%" },
       }}
     >
-      <LeftCardLogo theme={theme} />
+      <LeftCardLogo theme={theme} isAdmin={isAdmin} isVituaxUser={isVituaxUser} />
       <Divider sx={{ margin: "40px 0", background: theme[mode].grayLight }} />
-      <LeftCardDomain theme={theme} />
+      <LeftCardDomain theme={theme} isAdmin={isAdmin} isVituaxUser={isVituaxUser} />
     </Stack>
   );
 };
